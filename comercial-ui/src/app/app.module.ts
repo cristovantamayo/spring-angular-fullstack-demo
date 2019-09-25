5964ee92cb02cf87e4cb631d7269b875c5d32272
Cristovan Tamayo <cristovan.tamayo@gmail.com>
2019-09-25 14:47:23 -0300

    Angular Frontend - Final deployment of simple functionality with the Java Backend

--- comercial-ui/src/app/app.module.ts
+++ comercial-ui/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PainelNegociacaoComponent } from './painel-negociacao/painel-negociacao.component'
+
+import { TableModule } from 'primeng/table';
+import { PanelModule } from 'primeng/panel';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PainelNegociacaoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+
+    TableModule,
+    PanelModule,
+    InputTextModule,
+    ButtonModule,
+    ToastModule,
   ],
-  providers: [],
+  providers: [MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
